d36dc12a41f715d609244ebc5f7523f35559846e
huuquangnhatnguyen <nguye2hq@mail.uc.edu>
2024-01-30 15:09:37 +0700

    navigation bar update
    Updates on navigation bar

--- scripts/app.js
+++ scripts/app.js
@@ -1,37 +1,3 @@
-const storage = (table) => {
-
-    if (!localStorage.getItem(table)) {
-        localStorage.setItem(table, JSON.stringify({}));
-    }
-
-    const get = (key = null) => {
-        let data = JSON.parse(localStorage.getItem(table));
-        return key ? data[key] : data;
-    };
-
-    const set = (key, value) => {
-        let storage = get();
-        storage[key] = value;
-        localStorage.setItem(table, JSON.stringify(storage));
-    };
-
-    const unset = (key) => {
-        let storage = get();
-        delete storage[key];
-        localStorage.setItem(table, JSON.stringify(storage));
-    };
-
-    const has = (key) => Object.keys(get()).includes(key);
-
-    return {
-        get,
-        set,
-        unset,
-        has,
-    };
-};
-
-
 const timer = () => {
     let countDownDate = (new Date(document.getElementById('time-countdown').getAttribute('data-time').replace(' ', 'T'))).getTime();
 
