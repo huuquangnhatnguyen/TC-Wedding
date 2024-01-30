392e75dfb971071978b2c9410a7f1158fcb395b1
huuquangnhatnguyen <nguye2hq@mail.uc.edu>
2024-01-30 20:09:47 +0700

    countdown timer update
    fixed code bug ==> now the timer is runnable!

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,30 @@
-const timer = () => {
-    let countDownDate = (new Date(document.getElementById('time-countdown').getAttribute('data-time').replace(' ', 'T'))).getTime();
 
-    setInterval(() => {
-        let distance = Math.abs(countDownDate - (new Date()).getTime());
 
-        document.getElementById('days').innerText = Math.floor(distance / (1000 * 60 * 60 * 24));
-        document.getElementById('hours').innerText = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        document.getElementById('mins').innerText = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        document.getElementById('secs').innerText = Math.floor((distance % (1000 * 60)) / 1000);
-    }, 1000);
+var element = document.getElementById('time-countdown');
+var dataTime = element.getAttribute('data-time');
+let countDownDate = (new Date(dataTime)).getTime();
+console.log(countDownDate);
+// Update the countdown every second
+const countdownInterval = setInterval(timer, 1000);
+
+function timer () {
+        const currentDate = (new Date()).getTime();
+        let timeRemaining = Math.abs(countDownDate - currentDate);
+
+        let days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+        document.getElementById('days').innerHTML = `${days} `;
+
+        let hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        document.getElementById('hours').innerHTML = `${hours}`;
+        
+        let mins = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+        document.getElementById('mins').innerHTML = `${mins}`;
+
+        let secs = Math.floor((timeRemaining % (1000 * 60)) / 1000)
+        document.getElementById('secs').innerHTML = `${secs}`;
+
+        if (timeRemaining <= 0) {
+            clearInterval(countdownInterval);
+            document.getElementById('countdown').innerHTML = 'Countdown expired!';
+        }
 };
