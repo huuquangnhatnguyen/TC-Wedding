db37b16a7746e6f22b560a059d8e606506bdaeaa
huuquangnhatnguyen <nguye2hq@mail.uc.edu>
2024-01-31 03:36:17 +0700

    Update Photo Gallery
    All photos uploaded first time with modal to view all and some default photos

--- scripts/app.js
+++ scripts/app.js
@@ -120,4 +120,34 @@ function saveMessage(name, message) {
     // Remove the message from the DOM
     messageElement.remove();
   }
+
+  function showAllPhotos() {
+    var allPhotoGallery = document.getElementById('allPhotos')
+    for (let i = 1; i <= 44; i++) {
+        var newPhoto = document.createElement('div');
+        newPhoto.className = 'photo';
+        newPhoto.id = 'photo'+i;
+        newPhoto.innerHTML = '<img src="./images/portrait/portrait_'+i+'.jpg" alt=""></img>';
+        allPhotoGallery.appendChild(newPhoto);
+    }
+
+    for (let i = 1; i <= 16; i++) {
+        var newPhoto = document.createElement('div');
+        newPhoto.className = 'photo';
+        newPhoto.id = 'photo'+(i+22);
+        newPhoto.innerHTML = '<img src="./images/landscape/landscape_'+i+'.jpg" alt=""></img>';
+        allPhotoGallery.appendChild(newPhoto);
+    }
+
+    // Display the modal
+    document.getElementById('myModal').style.display = 'block';
+
+  }
+  
+  function closeModal() {
+    // Close the modal
+    document.getElementById('myModal').style.display = 'none';
+  }
+  
+
 console.log('run without bug')
