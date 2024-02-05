877ddb9ab0d03595eaa05d0c13fb0b8af3c3b620
huuquangnhatnguyen <nguye2hq@mail.uc.edu>
2024-02-05 11:40:57 +0700

    fetching data bug fixed + design adjustment

--- scripts/wish-form.js
+++ scripts/wish-form.js
@@ -1,21 +1,24 @@
 // Firebase Set up:
 // Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+
+import {getDatabase, ref, child, get} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBVz534wQmeoXn6SxBLWo4NlFaJeUBwpJM",
-  authDomain: "tc-wedding-36a90.firebaseapp.com",
-  databaseURL: "https://tc-wedding-36a90-default-rtdb.firebaseio.com",
-  projectId: "tc-wedding-36a90",
-  storageBucket: "tc-wedding-36a90.appspot.com",
-  messagingSenderId: "824076036510",
-  appId: "1:824076036510:web:23c1effad8dfecfe528e8f",
-  measurementId: "G-9Z7P0QKX2Z"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-// Get a reference to the database
-var database = firebase.database();
+  const firebaseConfig = {
+    apiKey: "AIzaSyBVz534wQmeoXn6SxBLWo4NlFaJeUBwpJM",
+    authDomain: "tc-wedding-36a90.firebaseapp.com",
+    databaseURL: "https://tc-wedding-36a90-default-rtdb.firebaseio.com",
+    projectId: "tc-wedding-36a90",
+    storageBucket: "tc-wedding-36a90.appspot.com",
+    messagingSenderId: "824076036510",
+    appId: "1:824076036510:web:23c1effad8dfecfe528e8f",
+    measurementId: "G-9Z7P0QKX2Z"
+  };
+  // Initialize Firebase
+  initializeApp(firebaseConfig);
+  // Get a reference to the database
+  var database = getDatabase();
 
 // Event listener for submission
 
@@ -41,7 +44,7 @@ function submissionMessage(event) {
     document.getElementById('RSVP').value = '';
     document.getElementById('form-message').value = '';
   }
-  console.log('succeeded')
+  console.log('succeeded submit')
 };
 
 // Function to add a new message to Firebase
@@ -51,31 +54,29 @@ function addMessageToFirebase(name, message, yesNoValue) {
     message: message,
     yesNoValue: yesNoValue
   });
-  console.log("i am here 3");
 }
 
 // Fetch Message from Database
 function fetchMessages() {
   console.log("i am here 4.1");
-  var wishRef = database.ref("TC wedding")
-  wishRef.once("value", function (snapshot) {
-    console.log("i am here 4");
-    var messagesContainer = document.getElementById('wish-container');
-    messagesContainer.innerHTML = ''; // Clear the existing messages
-
-    // Display existing messages
-    snapshot.forEach(
-      function (childSnapshot) {
-        var messageData = childSnapshot.val();
-        var messageId = childSnapshot.key;
-        var messageElement = createMessageElement(
-          messageData.name, messageData.message, messageData.yesNoValue, messageId
-          );
-        messagesContainer.appendChild(messageElement);
-      });
-      console.log("i am here 5");
-  });
+  const wishRef = ref(database);
+  get(child(wishRef, 'TC Wedding/')). then((snapshot) => {
+  var messagesContainer = document.getElementById('wish-container');
+  messagesContainer.innerHTML = ''; // Clear the existing messages
+  // Display existing messages
+  snapshot.forEach(
+    function (childSnapshot) {
+      var messageData = childSnapshot.val();
+      var messageId = childSnapshot.key;
+      var messageElement = createMessageElement(
+        messageData.name, messageData.message, messageData.yesNoValue, messageId
+        );
+      messagesContainer.appendChild(messageElement);
+    }
+    )}
+  )
 }
+
 
 // Function to create a message element
 function createMessageElement(name, message, yesNoValue, messageId) {
@@ -84,10 +85,9 @@ function createMessageElement(name, message, yesNoValue, messageId) {
   messageElement.className = 'wish-ans grid-box font-don-gian';
   messageElement.innerHTML = 
   '<div>'+
-    '<div class="guestname">' + name + '</div>'+ 
+    '<div class="guest-name">' + name + '</div>'+ 
     '<div class="guest-message">' + message + '</div>'+
-  '</div>' + 
-  '<button onclick="deleteMessage(\'' + messageId + '\')">X</button>';
+  '</div>';
   console.log("i am here 6");
   return messageElement;
 }
